d553f6e4af42d8cda807cfb66dba1e327a635996
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-02-21 00:01:42 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,1 @@
-user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
-
-user2 = User.create(:username => "flatiron4lyfe", :password => "Rubie!", :balance => 500)
-
-user3 = User.create(:username => "kittens1265", :password => "crazycatlady", :balance => 10000)
+Recipe1 = Recipe.create(:name => "skittles123", :ingredients => "iluvskittles", :cooking_time => 1000)
